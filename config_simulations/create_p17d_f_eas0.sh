04764f2a8b60ec22deada4db9d98d6b63322f316
Benjamin Grandey <benjamin.grandey@gmail.com>
2017-08-10 20:33:45 +0800

    First draft of config_f_simulations.org
    Using to create first draft of create_p17d_f_2000.sh and
    create_p17d_f_eas0.sh

---
+++ config_simulations/create_p17d_f_eas0.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CASENAME=p17d_f_eas0
+
+cd ~/models/cesm1_2_2_1/scripts/
+./create_newcase -case ~/cesm1_2_2_1_cases/$CASENAME -res f19_g16 -compset FC5 -mach cheyenne -pes_file pes/p720n20.xml
+
+cd ~/cesm1_2_2_1_cases/$CASENAME
+./cesm_setup
+wget https://raw.githubusercontent.com/grandey/p17d-sulphur-eas-eqm/master/user_nl_cam/user_nl_cam_$CASENAME
+mv user_nl_cam_$CASENAME user_nl_cam
+./$CASENAME.build
+ln -s /glade/scratch/bgrandey/$CASENAME/run/ scratch_run
+./xmlchange STOP_OPTION=nyears
+./xmlchange STOP_N=8
+./xmlchange RESUBMIT=3
+sed -i 's/regular/economy/g' $CASENAME.run
+./$CASENAME.submit
