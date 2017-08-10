c5024eefbd021224252f929c92016efe0548a81c
Benjamin Grandey <benjamin.grandey@gmail.com>
2017-08-10 20:56:28 +0800

    First draft of config_b_simulations.org
    Using to produce create_p17d_b_2000.sh and create_p17d_b_eas0.sh.
    
    Also edits to config_f_simulations.org and README.org

---
+++ config_simulations/create_p17d_b_eas0.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CASENAME=p17d_b_eas0
+
+cd ~/models/cesm1_2_2_1/scripts/
+./create_newcase -case ~/cesm1_2_2_1_cases/$CASENAME -res f19_g16 -compset BC5CN -mach cheyenne -pes_file pes/a720o128n24.xml
+
+cd ~/cesm1_2_2_1_cases/$CASENAME
+./cesm_setup
+wget https://raw.githubusercontent.com/grandey/p17d-sulphur-eas-eqm/master/user_nl_cam/user_nl_cam_$CASENAME
+mv user_nl_cam_$CASENAME user_nl_cam
+./$CASENAME.build
+ln -s /glade/scratch/bgrandey/$CASENAME/run/ scratch_run
+./xmlchange STOP_OPTION=nyears
+./xmlchange STOP_N=10
+./xmlchange RESUBMIT=9
+sed -i 's/regular/economy/g' $CASENAME.run
+./$CASENAME.submit
