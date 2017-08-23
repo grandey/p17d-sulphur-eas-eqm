ae4c0c814390d92dbc9ba7894fdf5e1f91f5571f
Benjamin Grandey <benjamin.grandey@gmail.com>
2017-08-23 14:47:40 +0800

    Updating cdo_analysis_prelim to include f_2000-f_eas0b
    And updates to README.org

--- analysis_prelim/cdo_analysis_prelim_p17d.sh
+++ analysis_prelim/cdo_analysis_prelim_p17d.sh
@@ -5,7 +5,7 @@
 #
 # Usage:
 #   To be run on local machine:
-#     ./cdo_analysis_prelim_p17c.sh
+#     ./cdo_analysis_prelim_p17d.sh
 #
 # Author:
 #   Benjamin S. Grandey, 2017
@@ -34,26 +34,33 @@ done
 # 2. Prescribed-SST simulation differences
 # 2a. 2D fields
 CASENAME1="p17d_f_2000"
-CASENAME2="p17d_f_eas0"
 VARIABLE_LIST="FSNTOA FSNTOA_d1 FSNTOAC_d1 SWCF_d1 LWCF LWCF_d1 H2SO4_SRF"
 
-for VARIABLE in $VARIABLE_LIST
+for CASENAME2 in p17d_f_eas0 p17d_f_eas0b
 do
-    IN_FILE1="$OUT_DIR/s1.tm.$CASENAME1.cam.h0.$VARIABLE.nc"
-    IN_FILE2="$OUT_DIR/s1.tm.$CASENAME2.cam.h0.$VARIABLE.nc"
-    OUT_FILE="$OUT_DIR/s2.tm.$CASENAME1-$CASENAME2.cam.h0.$VARIABLE.nc"
-    cdo sub $IN_FILE1 $IN_FILE2 $OUT_FILE
+    for VARIABLE in $VARIABLE_LIST
+    do
+	IN_FILE1="$OUT_DIR/s1.tm.$CASENAME1.cam.h0.$VARIABLE.nc"
+	IN_FILE2="$OUT_DIR/s1.tm.$CASENAME2.cam.h0.$VARIABLE.nc"
+	OUT_FILE="$OUT_DIR/s2.tm.$CASENAME1-$CASENAME2.cam.h0.$VARIABLE.nc"
+	cdo sub $IN_FILE1 $IN_FILE2 $OUT_FILE
+    done
 done
 
 # 2b. Derived fields
 # Net RFP
-IN_FILE1="$OUT_DIR/s2.tm.p17d_f_2000-p17d_f_eas0.cam.h0.FSNTOA.nc"
-IN_FILE2="$OUT_DIR/s2.tm.p17d_f_2000-p17d_f_eas0.cam.h0.LWCF_d1.nc"
-TEMP_FILE="$OUT_DIR/temp.nc"
-OUT_FILE="$OUT_DIR/s2.tm.p17d_f_2000-p17d_f_eas0.cam.h0.cFNTOA.nc"
-cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE
-cdo expr,'cFNTOA=FSNTOA+LWCF_d1' $TEMP_FILE $OUT_FILE
-rm -f $TEMP_FILE
+CASENAME1="p17d_f_2000"
+
+for CASENAME2 in p17d_f_eas0 p17d_f_eas0b
+do
+    IN_FILE1="$OUT_DIR/s2.tm.$CASENAME1-$CASENAME2.cam.h0.FSNTOA.nc"
+    IN_FILE2="$OUT_DIR/s2.tm.$CASENAME1-$CASENAME2.cam.h0.LWCF_d1.nc"
+    TEMP_FILE="$OUT_DIR/temp.nc"
+    OUT_FILE="$OUT_DIR/s2.tm.$CASENAME1-$CASENAME2.cam.h0.cFNTOA.nc"
+    cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE
+    cdo expr,'cFNTOA=FSNTOA+LWCF_d1' $TEMP_FILE $OUT_FILE
+    rm -f $TEMP_FILE
+done
 
 # 2c. Area-weighted means
 for IN_FILE in $OUT_DIR/s2.*.nc
@@ -86,11 +93,20 @@ cdo merge $IN_FILE1 $IN_FILE2 $TEMP_FILE
 cdo expr,'cPRECT=PRECC+PRECL' $TEMP_FILE $OUT_FILE
 rm -f $TEMP_FILE
 
-# 2c. Area-weighted means
+# 3c. Area-weighted means
 for IN_FILE in $OUT_DIR/s3.*.nc
 do
     OUT_FILE=${IN_FILE%nc}fm.nc
     cdo fldmean $IN_FILE $OUT_FILE
+done
+
+echo "Finished analysis"
+
+# Print area-weighted means
+for IN_FILE in $OUT_DIR/*.fm.nc
+do
+    echo ${IN_FILE##*/}
+    cdo infov $IN_FILE 2>/dev/null | grep -v '0.0000\|nan\|2.1475\|Mean'
 done
 
 echo "Finished"
