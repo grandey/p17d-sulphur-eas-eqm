7effb8e687d2cce4ba80ee1133cd99222f7591dd
Benjamin Grandey <benjamin.grandey@gmail.com>
2018-03-14 17:38:37 +0800

    Including CLDHGH and TGCLDIWP in analysis
    README.org: syncing CLDHGH and TGCLDIWP to local machine.
    lib_draft2018a.py: adding CLDHGH and TGCLDIWP to variable dictionaries.
    figures_draft2018a.ipynb: producing CLDHGH and TGCLDIWP figures as
    “other” figures.

--- analysis_draft2018a/lib_draft2018a.py
+++ analysis_draft2018a/lib_draft2018a.py
@@ -97,6 +97,8 @@ def load_variable_long_dict():
                           'LWCF': r'$\Delta$ longwave cloud radiative effect',
                           'FSNTOA-FSNTOA_d1': r'$\Delta$ direct radiative effect',
                           'FSNTOAC_d1': r'$\Delta$ surface albedo radiative effect',
+                          'CLDHGH': r'$\Delta$ high cloud fraction',
+                          'TGCLDIWP': r'$\Delta$ ice water path',
                           'TS': r'$\Delta$ surface temperature',
                           'PRECC+PRECL': r'$\Delta$ total precipitation rate',
                           'OMEGA_ml19': r'$\Delta$ vertical velocity at level $19$ ' +
@@ -115,6 +117,8 @@ def load_variable_symbol_dict():
                             'LWCF': r'$\Delta CRE_\mathrm{LW}$',
                             'FSNTOA-FSNTOA_d1': r'$\Delta DRE_\mathrm{SW}$',
                             'FSNTOAC_d1': r'$\Delta SRE_\mathrm{SW}$',
+                            'CLDHGH': r'$\Delta$ high cloud fraction',
+                            'TGCLDIWP': r'$\Delta$ ice water path',
                             'TS': r'$\Delta T$',
                             'PRECC+PRECL': r'$\Delta R$',
                             'OMEGA_ml19': r'$\Delta \omega$',
@@ -132,6 +136,8 @@ def load_variable_units_dict():
                            'LWCF': r'W m$^{-2}$',
                            'FSNTOA-FSNTOA_d1': r'W m$^{-2}$',
                            'FSNTOAC_d1': r'W m$^{-2}$',
+                           'CLDHGH': r'$\times 10^{-3}$',
+                           'TGCLDIWP': r'g m$^{-2}$',
                            'TS': r'$^{\circ}$C',
                            'PRECC+PRECL': r'mm year$^{-1}$',
                            'OMEGA_ml19': r'$\times 10^{-3}$ Pa s$^{-1}$',
@@ -144,7 +150,9 @@ def load_variable_sf_dict():
     """
     Load dictionary containing scale-factors to apply to variables.
     """
-    variable_sf_dict = {'PRECC+PRECL': 1e3*60*60*24*365,  # m/s -> mm/year
+    variable_sf_dict = {'CLDHGH': 1e3,  # -> x10^-3
+                        'TGCLDIWP': 1e3,  # kg/m2 -> g/m2
+                        'PRECC+PRECL': 1e3*60*60*24*365,  # m/s -> mm/year
                         'OMEGA_ml19': 1000,  # Pa/s -> mPa/s
                         }
     return variable_sf_dict
